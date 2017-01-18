eb89bca7c9f475771fa49e6a01f06eb5955d2a97
Devin "GIT SMASH!" Jackson <jackson.t.devin@gmail.com>
2017-01-17 23:59:08 -0500

    init commit

---
+++ index.ios.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Navigator,
+  TouchableHighlight
+} from 'react-native';
+
+export default class Keltrist extends Component {
+  render() {
+    const routes = [
+      {title: 'First Scene', index: 0},
+      {title: 'Second Scene', index: 1},
+    ];
+    return (
+      <Navigator
+        initialRoute={routes[0]}
+        initialRouteStack={routes}
+        renderScene={(route, navigator) =>
+          <TouchableHighlight onPress={() => {
+            if (route.index === 0) {
+              navigator.push(routes[1]);
+            } else {
+              navigator.pop();
+            }
+          }}>
+          <Text>Hello {route.title}!</Text>
+          </TouchableHighlight>
+        }
+        style={{padding: 100}}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('Keltrist', () => Keltrist);
